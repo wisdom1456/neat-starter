96d0833b032adfb6cb1e49e2ad75ed3610d5f91e
wisdom1456 <Modible@gmail.com>
2021-05-18 08:52:59 -0500

    Update front page, and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,36 @@
 module.exports = {
-  purge: {
-    mode: "all",
-    content: ["./**/*.html"],
-    options: {
-      whitelist: [],
+    purge: {
+        mode: "all",
+        content: ["./**/*.html"],
+        options: {
+            whitelist: []
+        }
     },
-  },
-  theme: {
-    container: {
-      center: true,
+    theme: {
+        container: {
+            center: true
+        },
+        extend: {
+            colors: {
+                br: {
+                    100: "#E6F0FF",
+                    200: "#BFDAFF",
+                    300: "#17baa3",
+                    400: "#1764b1",
+                    500: "#1e4da2",
+                    600: "#155ba4",
+                    700: "#004098",
+                    800: "#003072",
+                    900: "#00204C"
+                }
+            }
+        },
+        backgroundImage: (theme) => ({
+            "hero-back": "url('/static/img/bg/columnsback2.jpg')",
+            "marble-back": "url('/static/img/bg/marbleback2.jpg')",
+            "footer-texture": "url('/static/img/footer-texture.png')"
+        })
     },
-    extend: {
-      colors: {},
-    },
-  },
-  variants: {},
-  plugins: [require("@tailwindcss/typography")],
+    variants: {},
+    plugins: [require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")]
 };
